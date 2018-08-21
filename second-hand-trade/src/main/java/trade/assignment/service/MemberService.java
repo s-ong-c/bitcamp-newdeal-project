94046409148bf3a12f7b969ac6ee1bc772da64a2
s-ong-c <somony9292@gmail.com>
2018-08-21 14:10:35 +0900

    index.html

--- second-hand-trade/src/main/java/trade/assignment/service/MemberService.java
+++ second-hand-trade/src/main/java/trade/assignment/service/MemberService.java
@@ -3,6 +3,7 @@ package trade.assignment.service;
 import trade.assignment.domain.Member;
 
 public interface MemberService {
+	
     int add(Member member);
 
     Member getMember(String email, String password);
--- second-hand-trade/src/main/java/trade/assignment/service/impl/MemberServiceImpl.java
+++ second-hand-trade/src/main/java/trade/assignment/service/impl/MemberServiceImpl.java
@@ -3,9 +3,6 @@ package trade.assignment.service.impl;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import trade.assignment.domain.Member;
