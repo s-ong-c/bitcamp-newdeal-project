b1933ad1f2f8165966cca5fb29e7bb002e3762ae
s-ong-c <somony9292@gmail.com>
2018-08-27 10:12:35 +0900

    우선edit 및 프로필 수정 창UI 완

--- second-hand-trade/src/main/webapp/html/js/edit.js
+++ second-hand-trade/src/main/webapp/html/js/edit.js
@@ -4,5 +4,6 @@ var serverApiAddr = "http://localhost:8080/second-hand-trade";
 
 
 $(() => {
-	$('._28rsa > header').load(`${serverApiAddr}/html/editSide.html`)
+	$('._28rsa > header').load(`${serverApiAddr}/html/editSide2.html`)
+	$('._75z9k > header2').load(`${serverApiAddr}/html/modifyProfilePhoto.html`)
 });
