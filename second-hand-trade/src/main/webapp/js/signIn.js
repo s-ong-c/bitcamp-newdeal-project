e584b67054c6c0d8418a1bd7a04da2b8accfbc7b
s-ong-c <somony9292@gmail.com>
2018-08-31 14:33:58 +0900

    push

--- second-hand-trade/src/main/webapp/js/signIn.js
+++ second-hand-trade/src/main/webapp/js/signIn.js
@@ -57,7 +57,7 @@ $('#loginBtn').click(() => {
         'saveEmail':$('#fSaveEmail').prop('checked')
     }, (result) => {
         if(result.status==='success'){
-        		location.href='html/main/mainTest.html'
+        		location.href='html/main/main.html'
         }else{
         	alert('로그인 실패')
         }
